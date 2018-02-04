6eebd78257bfae796972cb2930062a00cd0da336
qiuqiu <1184228534@qq.com>
2018-02-04 11:44:05 +0800

    Create intToString.cpp

---
+++ intToString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+string intToString(int num)
+{
+    ostringstream o;
+    if (o << x)
+        return o.str();
+    // 这儿进行一些错误处理...
+    return "conversion error";
+}
